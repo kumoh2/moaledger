9ed23b6077193a7f2e03ef2bcea121a59b3a1c52
Choi Jaeho <31762986+kumoh2@users.noreply.github.com>
2025-03-02 15:54:46 +0900

    Update backend folder

---
+++ backend/src/main/java/org/moa/ledger/MoaLedgerApplication.java
@@ -0,0 +1,13 @@
+package org.moa.ledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoaLedgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoaLedgerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/org/moa/ledger/MoaLedgerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.moa.ledger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoaLedgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
